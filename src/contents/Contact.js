db8ee941ffe3b61ef1bc099f4b99c36cf931a174
bew32 <80671223+bew32@users.noreply.github.com>
2022-06-15 09:54:42 +0700

    Update Contact.js

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -8,8 +8,8 @@ class Contact extends Component {
             <div className="condiv">
                 <h1 className="subtopic">Contact Me</h1>
                 <h3>Email : tawatninja425@gmail.com</h3>
-                {/* <h3>Email : tawatcity38@gmail.com</h3>
-                <h3>Email : 624259016@webmail.npru.ac.th</h3> */}
+                <h3>Email : tawatcity38@gmail.com</h3>
+                <h3>Email : 624259016@webmail.npru.ac.th</h3>
                 <h3>Instagram : @bew_tw</h3>
                 <h3>Youtube : BEW_KUNG</h3>
                 <Social />
